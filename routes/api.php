7891d57aae80fafca6b4f498f509c84a4d61fea8
FranklynRS <franklyn.santos@univale.br>
2025-05-20 15:45:03 -0300

    feat: criar rota palavra-chave

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PessoaController;
 use App\Http\Controllers\FuncaoController;
 use App\Http\Controllers\ObraController;
+use App\Http\Controllers\PalavraChaveController;
 
 Route::apiResource('pessoas', PessoaController::class);
 
@@ -11,3 +12,6 @@ Route::apiResource('funcoes', FuncaoController::class)
     ->parameters(['funcoes' => 'funcao']); //Isso foi necessário pois o laravel estava considerando o singular de 'funcoes' como 'funco'
 
 Route::apiResource('obras', ObraController::class);
+
+Route::apiResource('palavras-chave', PalavraChaveController::class)
+    ->parameters(['palavras-chave' => 'palavra_chave']);
