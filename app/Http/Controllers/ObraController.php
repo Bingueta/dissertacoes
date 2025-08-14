756ae4a0d19fe33424f89ecfed37c68302ce66b0
FranklynRS <franklyn.santos@univale.br>
2025-08-14 17:17:06 -0300

    docs: adiciona créditos à FAPEMIG e UNIVALE no README

--- app/Http/Controllers/ObraController.php
+++ app/Http/Controllers/ObraController.php
@@ -4,25 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Models\Obra;
 use Illuminate\Http\Request;
+use App\Models\RelObrasPessoas;
+use App\Models\RelObrasLocalizacoes;
+use App\Models\RelObrasPalavrasChave;
 
 class ObraController extends Controller
 {
-    public function index() //lista todas as obras
+    public function index()
     {
         return Obra::all();
     }
 
     public function store(Request $request)
     {
+        //dd($request->all()); isso era pra observar a estrutura do JSON
+
         $validated = $request->validate([
             'titulo' => 'required|string|max:300',
             'ano' => 'required|string|max:4',
             'acervo' => 'nullable|string|max:255',
             'link_pdf' => 'required|string|max:500',
             'resumo' => 'required|string',
+
+            'pessoas' => 'array|required',
+            'pessoas.*.id_pessoa' => 'required|integer|exists:pessoas,id_pessoa',
+            'pessoas.*.id_funcao' => 'required|integer|exists:fucoes,id_funcao',
+
+            'localizacoes' => 'array|required',
+            'localizacoes.*' => 'required|integer|exists:local_especifico,id_local_especifico',
+
+            'palavras_chave' => 'array|required',
+            'palavras_chave.*' => 'required|integer|exists:palavras_chave,id_palavra_chave',
+
         ]);
 
-        return Obra::create($validated);
+        $obra = Obra::create($validated);
+
+        foreach ($validated['pessoas'] as $pessoa) {
+            RelObrasPessoas::create([
+                'id_obra' => $obra->id_obra,
+                'id_pessoa' => $pessoa['id_pessoa'],
+                'id_funcao' => $pessoa['id_funcao'],
+            ]);
+        }
+
+        foreach ($validated['localizacoes'] as $localizacao) {
+            RelObrasLocalizacoes::create([
+                'id_obra' => $obra->id_obra,
+                'id_local_especifico' => $localizacao,
+            ]);
+        }
+
+        foreach ($validated['palavras_chave'] as $palavra_chave) {
+            RelObrasPalavrasChave::create([
+                'id_obra' => $obra->id_obra,
+                'id_palavra_chave' => $palavra_chave,
+            ]);
+        }
+
+        return $obra;
     }
 
     public function show(Obra $obra)
